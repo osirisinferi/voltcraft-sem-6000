e6ae75df0931d381e07ade86c28ed3ed849f1bc4
moormaster <moormaster@gmx.net>
2020-07-04 00:03:51 +0200

    read response for authentication

--- sem-6000.py
+++ sem-6000.py
@@ -3,20 +3,71 @@
 import sys
 from bluepy import btle
 
+class AuthorizationNotification:
+    def __init__(self, was_successful):
+        self.was_successful = was_successful
+
+    def __str__(self):
+        return "AuthorizationNotification(was_successful=" + str(self.was_successful) + ")"
+
 class SEM6000Delegate(btle.DefaultDelegate):
+    def __init__(self, is_debug=False):
+        btle.DefaultDelegate.__init__(self)
+
+        self.is_debug = False
+        if is_debug:
+            self.is_debug = True
+
+        self.last_notification = None
+
+    def _parse_payload(self, data):
+        if data[0:1] != b'\x0f':
+            raise Exception("Invalid response")
+
+        length_of_payload = data[1]
+
+        payload = data[2:2+length_of_payload-1]
+        checksum_received = data[2+length_of_payload-1]
+
+        checksum = (1+sum(payload)) & 0xff
+
+        if checksum_received != checksum:
+            raise Exception("Invalid checksum " + str(checksum) + ", expected=" + str(checksum_received))
+
+        if len(data) > 2+length_of_payload:
+            # if suffix exists it must be b'\xff\xff'
+            suffix = data[2+length_of_payload:]
+            if suffix != b'\xff\xff':
+                raise Exception("Invalid suffix " + str(suffix))
+
+        return payload
+
+    def _parse_notification(self, data):
+        payload = self._parse_payload(data)
+
+        if payload[0:2] == b'\x17\x00':
+            if len(payload) != 5:
+                raise Exception("invalid payload length for AuthenticationNotification")
+
+            was_successful = False
+            if payload[2:3] == b'\x00':
+                was_successful = True
+
+            return AuthorizationNotification(was_successful=was_successful)
+
+        return None
+
     def handleNotification(self, cHandle, data):
-        print("received data from handle " + str(cHandle) + ": " + str(data), file=sys.stderr)
+        if self.is_debug:
+            print("received data from handle " + str(cHandle) + ": " + str(data), file=sys.stderr)
 
-    def handleDiscovery(self, scanEntry, isNewDev, isNewData):
-        address = scanEntry.addr
-        # 0x02 - query "Incomplete List of 16-bit Service Class UUIDs"
-        service_class_uuids = scanEntry.getValueText(2)
-        # 0x09 - query complete local name
-        complete_local_name = scanEntry.getValueText(9)
+        self.last_notification = self._parse_notification(data)
 
-        if service_class_uuids == "0000fff0-0000-1000-8000-00805f9b34fb":
-            # only notify about sem6000 devices
-            print({'address': address, 'name': complete_local_name}, file=sys.stderr)
+        if self.is_debug:
+            if not self.last_notification is None:
+                print(self.last_notification, file=sys.stderr)
+            else:
+                print("Unknown notification received", file=sys.stderr)
 
 
 class SEM6000():
@@ -29,7 +80,8 @@ class SEM6000():
             for i in pin:
                 self.pin += int(i).to_bytes(1, 'little')
 
-        self._peripheral = btle.Peripheral(deviceAddr=deviceAddr, addrType=btle.ADDR_TYPE_PUBLIC, iface=iface).withDelegate(SEM6000Delegate())
+        self._delegate = SEM6000Delegate(is_debug=True)
+        self._peripheral = btle.Peripheral(deviceAddr=deviceAddr, addrType=btle.ADDR_TYPE_PUBLIC, iface=iface).withDelegate(self._delegate)
         self._characteristics = self._peripheral.getCharacteristics(uuid='0000fff3-0000-1000-8000-00805f9b34fb')[0]
 
         self.authorize()
@@ -58,6 +110,10 @@ class SEM6000():
     def authorize(self):
         command = self._create_authorize_command()
         self._send_command(command)
+        notification = self._delegate.last_notification
+
+        if not isinstance(notification, AuthorizationNotification) or not notification.was_successful:
+            raise Exception("Authentication failed")
 
     def power_on(self):
         command = self._create_power_on_command()
